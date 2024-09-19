79c4204fc04b2df8501fcd0b9c3ec86884ebf35d
Louis Escher <louisescher@proton.me>
2024-09-19 19:38:52 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://louisescher.github.io',
+  base: '/studiocms-login-test',
   vite: {
     optimizeDeps: {
       exclude: ['three']
